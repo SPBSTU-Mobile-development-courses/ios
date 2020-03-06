a22010488e7e7f9881652985380cc063211e8fb7
antonnazarov <nazarof.anton@gmail.com>
2020-03-07 00:31:52 +0300

    Rick&Morty initial project added

---
+++ 2020-spring/lectures/7.03/RickAndMorty/RickAndMorty/AppDelegate.swift
@@ -0,0 +1,16 @@
+//
+//  AppDelegate.swift
+//  RickAndMorty
+//
+//  Created by Anton Nazarov on 06.03.2020.
+//  Copyright © 2020 Anton Nazarov. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+}
