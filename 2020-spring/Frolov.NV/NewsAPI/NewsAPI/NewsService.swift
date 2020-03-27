c5feef01e9da84d6528911dd184fef1c02be0683
nikitafrolov <nickit.froloff2016@yandex.ru>
2020-03-27 19:06:42 +0300

    Добавил URLComponents и испрвил оишбку completion воть.

---
+++ 2020-spring/Frolov.NV/NewsAPI/NewsAPI/NewsService.swift
@@ -0,0 +1,60 @@
+//
+//  NewsService.swift
+//  NewsAPI
+//
+//  Created by Никита Фролов  on 25.03.2020.
+//  Copyright © 2020 Никита Фролов . All rights reserved.
+//
+
+import Foundation
+
+
+class NewsService {
+    private var pageNumber:Int = 1;
+    
+    private var urlComponents = URLComponents(string: "https://newsapi.org")
+    private var nextUrl: URL?
+    
+    public func getNews(completion: @escaping ([OneNew]?)-> Void) {
+        urlComponents?.path = "/v2/top-headlines"
+        urlComponents?.queryItems = [
+            URLQueryItem(name: "country", value: "us"),
+            URLQueryItem(name: "page", value: "1"),
+            URLQueryItem(name: "apiKey", value: "e5a2d92c79b74e8aa40a9e618e6a1998")
+        ]
+        guard let url = urlComponents?.url else {
+            completion(nil)
+            return
+        }
+        getNews(url: url,completion: completion)
+    }
+    
+    public func getMoreNews(complition: @escaping ([OneNew]?)-> Void) {
+        guard let url = nextUrl else {
+            complition(nil)
+            return
+        }
+        getNews(url: url, completion: complition)
+    }
+    
+    private func getNextPage()-> URL{
+        pageNumber += 1;
+        urlComponents?.queryItems?[1].value = String(pageNumber)
+        guard let newUrl = urlComponents?.url else {
+            fatalError("Problem with URL")
+        }
+        return newUrl
+    }
+    
+    private func getNews(url: URL, completion: @escaping ([OneNew]?)->Void) {
+        URLSession.shared.dataTask(with: url) { (data, response, error) in
+            guard let data = data else {
+                completion(nil)
+                return
+            }
+            let articals = try? JSONDecoder().decode(Articals<OneNew>.self, from: data)
+            self.nextUrl = self.getNextPage()
+            completion(articals?.articles)
+        }.resume()
+    }
+}
