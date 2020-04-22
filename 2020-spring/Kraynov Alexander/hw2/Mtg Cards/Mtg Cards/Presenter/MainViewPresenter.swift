d65bbf47811726510b0707d0292c18c55831f68a
Alexander Kraynov <kraynovsasha@gmail.com>
2020-04-22 22:52:33 +0300

    added MVP architecture

---
+++ 2020-spring/Kraynov Alexander/hw2/Mtg Cards/Mtg Cards/Presenter/MainViewPresenter.swift
@@ -0,0 +1,64 @@
+//
+//  MainViewPresenter.swift
+//  Mtg Cards
+//
+//  Created by Admin on 22.04.2020.
+//  Copyright © 2020 alexander. All rights reserved.
+//
+
+import DeepDiff
+import UIKit
+
+class MainViewPresenter {
+    typealias OnUpdateCompletion = ([Card]?) -> Void
+
+    private var cards = [Card]()
+    private let view: MainViewController
+    private let cardFacade: CardFacade = CardFacadeImpl(cardService: CardServiceImpl(), cardRepository: CardRepositoryImpl())
+
+    init(view: MainViewController) {
+        self.view = view
+        cardFacade.getCards { cardArray in
+            guard let cardArray = cardArray else {
+                return
+            }
+            self.cards = cardArray
+            view.filteredData = self.cards
+            view.reloadTableView()
+        }
+    }
+    func getCards(completion: @escaping OnUpdateCompletion) {
+        cardFacade.getCards { cardArray in
+            guard let cardArray = cardArray else {
+                return
+            }
+            self.cards = cardArray
+            self.view.filteredData = self.cards
+            completion(self.cards)
+        }
+    }
+    func loadMore(completion: @escaping OnUpdateCompletion) {
+        cardFacade.loadMore(completion: completion)
+    }
+
+    func getDiff(old: [Card], new: [Card]) -> ([IndexPath], [IndexPath]) {
+        let differences = diff(old: old, new: new)
+        var deletions = [IndexPath]()
+        var insertions = [IndexPath]()
+        for difference in differences {
+            if let deletion = difference.delete?.index {
+                deletions.append(IndexPath(row: deletion, section: 0))
+            }
+            if let insertion = difference.insert?.index {
+                insertions.append(IndexPath(row: insertion, section: 0))
+            }
+        }
+       return (insertions, deletions)
+    }
+
+    func filter(with query: String) -> [Card] {
+        query.isEmpty ? cards : cards.filter {
+            $0.name.lowercased().contains(query.lowercased())
+        }
+    }
+}
